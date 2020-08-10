d5b2a5a3127101a4037fa52a646d374ce504dcbb
Nex Sabre <NexSabre@users.noreply.github.com>
2020-08-10 18:33:05 +0200

    Remove unused code

--- src/pip2spack/actions/dowload/download_action.py
+++ src/pip2spack/actions/dowload/download_action.py
@@ -1,5 +1,4 @@
 from pip2spack.actions.action import Action
-from pip2spack.framework.helpers import create_directory, create_package
 from pip2spack.framework.messages import Messages
 from pip2spack.core.pypi_package import PyPiPackage
 from pip2spack.core.verification import Verification
@@ -14,7 +13,6 @@ class DownloadAction(Action):
 
     def process_action(self, configuration):
         ready_packages = Verification(configuration.name).available_packages
-        # self.generate_packages(ready_packages)
         self.download(ready_packages)
 
     def download(self, ready_packages):
@@ -23,22 +21,3 @@ class DownloadAction(Action):
 
             d_package = PyPiPackage(value)
             d_package.download_versions()
-
-
-    @staticmethod
-    def generate_packages(ready_packages):
-        for key, value in ready_packages.items():
-            Messages.info(f"Generating package for the {key}")
-
-            s_package = PyPiPackage(value)
-            generated_file = s_package.generate_file()
-
-            create_directory(key)
-            created_package_uri = create_package(key, generated_file)
-            print(f" OK  :: Package for {key} was generated\n\t "
-                  f":: If \'spack install py-{key}\' return any problems, try to uncomment:\n\t "
-                  f":: \tdepends_on('py-setuptools', type='build')\n\t "
-                  f":: at {created_package_uri}")
-
-
-
